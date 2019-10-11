938f0ce29fa281d7ecfaf3db1d5b404ec7f1e3f9
Eric-Weng <weng.eric8@gmail.com>
2019-10-11 00:18:26 -0600

    Added Footer
    Added:
    Footer, Back to top button.
    
    Fixed:
    Join community container, shop for games button.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,25 @@ const selectElement = (s) => document.querySelector(s);
 selectElement('.open').addEventListener('click', () => {
     selectElement('.nav-list').classList.add('active');
 });
+
 //Close menu
 selectElement('.close').addEventListener('click', () => {
     selectElement('.nav-list').classList.remove('active');
+});
+
+$('nav a[href*="#"]').on('click',function(){
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 1100);
+});
+
+  $('#up').on('click',function(){
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1100);
+});
+AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
 });
